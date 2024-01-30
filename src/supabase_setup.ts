4d91bc3d28700ebe20a04358eab796c4bec2f3b2
abhi17Ab <abhishekgopalt2004@gmail.com>
2024-01-30 08:58:59 +0530

    Update supabase_setup.ts

--- src/supabase_setup.ts
+++ src/supabase_setup.ts
@@ -1,8 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "./models/supabase";
+import {createClient} from "@supabase/supabase-js";
+import {Database} from "./models/supabase";
 
-// Replace placeholders with your actual Supabase credentials
-const supabaseUrl = "https://hoxgwelczfqetztuklcv.supabase.co";
-const supabaseApIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhveGd3ZWxjemZxZXR6dHVrbGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MzcyODYsImV4cCI6MjAyMjExMzI4Nn0.UOyh4dJBTOECTSd0Oy2vDvkdhbTYG8_MFzJbLmvBMGM";
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseApIKey);
+export const supabase =
+    createClient<Database>(import.meta.env.VITE_SUPABASE_URL("https://hoxgwelczfqetztuklcv.supabase.co"), import.meta.env.VITE_SUPABASE_APIKEY("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhveGd3ZWxjemZxZXR6dHVrbGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MzcyODYsImV4cCI6MjAyMjExMzI4Nn0.UOyh4dJBTOECTSd0Oy2vDvkdhbTYG8_MFzJbLmvBMGM"));
